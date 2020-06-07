4577d203ad524b587cdc0d325668f8a2b4eb6286
QingCi <39399831+yuanqingx@users.noreply.github.com>
2020-06-07 22:26:26 +0800

    feat: find convert from any object

--- src/com/qingci/lazy/FindConverterAction.java
+++ src/com/qingci/lazy/FindConverterAction.java
@@ -10,12 +10,13 @@ import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
+import com.intellij.psi.impl.source.PsiClassReferenceType;
+import com.intellij.psi.impl.source.PsiImmediateClassType;
+import com.intellij.psi.impl.source.tree.java.PsiExpressionStatementImpl;
+import com.intellij.psi.impl.source.tree.java.PsiMethodCallExpressionImpl;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.PsiShortNamesCache;
 import org.apache.commons.lang3.StringUtils;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class FindConverterAction extends AnAction {
 
@@ -23,40 +24,37 @@ public class FindConverterAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         Project project = e.getProject();
         SelectionModel selectionModel = e.getData(PlatformDataKeys.EDITOR).getSelectionModel();
-        PsiFile file = e.getData(CommonDataKeys.PSI_FILE);
+        PsiFile currentPsiFile = e.getData(CommonDataKeys.PSI_FILE);
 
         selectionModel.selectLineAtCaret();
-        String line = selectionModel.getSelectedText();
+        String line = selectionModel.getSelectedText().replace("\n", "").trim();
         selectionModel.removeSelection();
 
-        String regex = "convert[(].*\\.class";
-        Pattern r = Pattern.compile(regex);
-        Matcher matcher = r.matcher(line);
-        if (matcher.find()) {
-            String convert = matcher.group();
-            String paramStr = convert.substring(convert.indexOf("(") + 1, convert.lastIndexOf("."));
-            String replace = paramStr.replace(" ", "");
-            String[] params = replace.split(",");
-            String source = params[0];
-            String target = params[1];
+        currentPsiFile.accept(new JavaRecursiveElementVisitor() {
+            @Override
+            public void visitStatement(PsiStatement statement) {
+                super.visitStatement(statement);
+                if (StringUtils.equals(line, statement.getText())) {
+                    // 当前程序语句中被调用的方法对应一个表达式对象
+                    // 获取当前执行语句的表达式对象
+                    PsiExpression expression = ((PsiExpressionStatementImpl) statement).getExpression();
+                    // 获取当表达式的参数列表对象中参数表达式(数组，每一个参数也有对应的表达式对象)
+                    PsiExpression[] expressions = ((PsiMethodCallExpressionImpl) expression).getArgumentList().getExpressions();
+                    PsiType sourceType = expressions[0].getType();
+                    PsiType targetType = ((PsiImmediateClassType) expressions[1].getType()).getParameters()[0];
 
-            file.accept(new JavaRecursiveElementVisitor() {
-                @Override
-                public void visitLocalVariable(PsiLocalVariable variable) {
-                    super.visitLocalVariable(variable);
-                    if (StringUtils.equals(variable.getName(), source)) {
-                        PsiTypeElement typeElement = variable.getTypeElement();
-                        PsiClass[] classes = PsiShortNamesCache.getInstance(project).getClassesByName(typeElement.getText() + "2" + target + "Converter", GlobalSearchScope.projectScope(project));
-                        PsiClass aClass = classes[0];
-                        VirtualFile virtualFile = aClass.getOriginalElement().getContainingFile().getVirtualFile();
-                        ApplicationManager.getApplication()
-                                .invokeLater(() -> FileEditorManager.getInstance(project).openFile(virtualFile, true, true));
-                    }
-
+                    String sName = ((PsiImmediateClassType) sourceType).getName();
+                    String tName = ((PsiClassReferenceType) targetType).getName();
+                    PsiClass[] classes = PsiShortNamesCache.getInstance(project).getClassesByName(sName + "2" + tName + "Converter", GlobalSearchScope.projectScope(project));
+                    PsiClass aClass = classes[0];
+                    VirtualFile virtualFile = aClass.getOriginalElement().getContainingFile().getVirtualFile();
+                    ApplicationManager.getApplication()
+                            .invokeLater(() -> FileEditorManager.getInstance(project).openFile(virtualFile, true, true));
                 }
-            });
 
-        }
+            }
+
+        });
 
     }
 
